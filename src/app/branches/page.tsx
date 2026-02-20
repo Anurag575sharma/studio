c2fa13c0c6447d6fef63e12b0930e8f7d102535d
Anurag sharma <anuragsharma575dta@gmail.com>
2026-02-20 17:15:05 +0000

    Try fixing this error: `Build Error: Ecmascript file had an error. Error

--- src/app/branches/page.tsx
+++ src/app/branches/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import Image from "next/image";
 import { SpotlightCard } from "@/components/branches/spotlight-card";
 import { SectionWrapper } from "@/components/shared/section-wrapper";
