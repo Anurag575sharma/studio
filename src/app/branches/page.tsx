b2d333f13dc70ae6ce59fc8879be727dd37b0150
Anurag sharma <anuragsharma575dta@gmail.com>
2026-02-20 16:13:27 +0000

    Try fixing this error: `Console Error: Only plain objects can be passed

--- src/app/branches/page.tsx
+++ src/app/branches/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from "next/image";
 import { SpotlightCard } from "@/components/branches/spotlight-card";
 import { SectionWrapper } from "@/components/shared/section-wrapper";
